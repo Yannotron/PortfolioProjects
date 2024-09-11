972c38330abbb53b02d53e727e6c4146fa5512a9
Yann <72335936+Yannotron@users.noreply.github.com>
2024-09-11 15:32:03 +0100

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[rolling people vaccinated]
+  FROM [Portfolio Project].[dbo].[PercentPopulationVaccinated]
